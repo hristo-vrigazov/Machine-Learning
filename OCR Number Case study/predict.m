6690c049817dadcb876b8bc5c0e27001a94b5869
Your Name <hvrigazov@gmail.com>
2015-11-01 14:49:27 +0300

    prediction ocr

--- OCR Number Case study/predict.m
+++ OCR Number Case study/predict.m
@@ -21,7 +21,11 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
-a1 = [ones(size(X, 1)) X];
+a1 = [ones(m, 1) X];
+z2 = sigmoid(a1 * Theta1');
+a2 = [ones(size(z2), 1) z2];
+a3 = sigmoid(a2 * Theta2');
+[v p] = max(a3, [], 2);
 
 % =========================================================================
 
